0331c30d418b6959e52b55de130e8d33f84b82f4
Luki <lukioff@protonmail.com>
2025-06-04 03:58:12 +0300

    Add Settings

--- Multi Saves Backup Tool/App.axaml.cs
+++ Multi Saves Backup Tool/App.axaml.cs
@@ -1,14 +1,12 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
 using Multi_Saves_Backup_Tool.ViewModels;
 using Multi_Saves_Backup_Tool.Views;
 
 namespace Multi_Saves_Backup_Tool;
 
-public class App : Application
+public partial class App : Application
 {
     public override void Initialize()
     {
@@ -19,28 +17,14 @@ public class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel(),
-            };
+            var mainWindow = new MainWindow();
+            var mainViewModel = new MainWindowViewModel(mainWindow);
+            
+            mainWindow.DataContext = mainViewModel;
+            desktop.MainWindow = mainWindow;
         }
 
         base.OnFrameworkInitializationCompleted();
     }
-
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
-    }
 }
+
--- Multi Saves Backup Tool/ViewModels/MainWindowViewModel.cs
+++ Multi Saves Backup Tool/ViewModels/MainWindowViewModel.cs
@@ -1,83 +1,55 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reactive.Linq;
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
 using FluentAvalonia.UI.Controls;
-using ReactiveUI;
 
 namespace Multi_Saves_Backup_Tool.ViewModels;
 
-public class MainWindowViewModel : ReactiveObject
+public partial class MainWindowViewModel : ViewModelBase
 {
-    private NavigationViewItem? _selectedMenuItem;
+    [ObservableProperty]
     private ViewModelBase? _currentViewModel;
-    
-    public NavigationViewItem? SelectedMenuItem
-    {
-        get => _selectedMenuItem;
-        set => this.RaiseAndSetIfChanged(ref _selectedMenuItem, value);
-    }
-    
-    public ViewModelBase? CurrentViewModel
-    {
-        get => _currentViewModel;
-        set => this.RaiseAndSetIfChanged(ref _currentViewModel, value);
-    }
-    
+
+    [ObservableProperty]
+    private NavigationViewItem? _selectedMenuItem;
+
     public MonitoringViewModel MonitoringViewModel { get; }
     public GamesViewModel GamesViewModel { get; }
     public ArchivesViewModel ArchivesViewModel { get; }
-    
+    public SettingsViewModel SettingsViewModel { get; }
+
+    public override string Title => "Multi Saves Backup Tool";
+
     public MainWindowViewModel()
+    {
+        var window = new Window();
+        MonitoringViewModel = new MonitoringViewModel();
+        GamesViewModel = new GamesViewModel();
+        ArchivesViewModel = new ArchivesViewModel();
+        SettingsViewModel = new SettingsViewModel(window.StorageProvider);
+        CurrentViewModel = MonitoringViewModel;
+    }
+
+    public MainWindowViewModel(Window mainWindow)
     {
         MonitoringViewModel = new MonitoringViewModel();
         GamesViewModel = new GamesViewModel();
         ArchivesViewModel = new ArchivesViewModel();
-        
-        this.WhenAnyValue(x => x.SelectedMenuItem)
-            .Where(item => item != null)
-            .Subscribe(OnNavigationChanged);
-        
+        SettingsViewModel = new SettingsViewModel(mainWindow.StorageProvider);
         CurrentViewModel = MonitoringViewModel;
     }
-    
-    private void OnNavigationChanged(NavigationViewItem? selectedItem)
+
+    partial void OnSelectedMenuItemChanged(NavigationViewItem? oldValue, NavigationViewItem? newValue)
     {
-        var tag = selectedItem?.Tag?.ToString();
-        
-        CurrentViewModel = tag switch
+        if (newValue == null) return;
+
+        CurrentViewModel = (newValue.Tag as string) switch
         {
             "monitoring" => MonitoringViewModel,
             "games" => GamesViewModel,
             "archives" => ArchivesViewModel,
-            _ => MonitoringViewModel
+            "settings" => SettingsViewModel,
+            _ => CurrentViewModel
         };
-        
-        switch (tag)
-        {
-            case "monitoring":
-                OnMonitoringSelected();
-                break;
-            case "games":
-                OnGamesSelected();
-                break;
-            case "archives":
-                OnArchivesSelected();
-                break;
-        }
-    }
-    
-    private void OnMonitoringSelected()
-    {
-        Debug.WriteLine("Переключение на Мониторинг");
-    }
-    
-    private void OnGamesSelected()
-    {
-        Debug.WriteLine("Переключение на Игры");
-    }
-    
-    private void OnArchivesSelected()
-    {
-        Debug.WriteLine("Переключение на Архивы");
     }
 }
+
